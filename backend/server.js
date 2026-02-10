1b2f8059bae10c58a9133908d8d82c3d736b3714
Nikhil Hiwale <nikhilhiwale29@gmail.com>
2026-02-10 19:38:28 +0530

    Add GET test route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
+app.get("/api/auth", (req, res) => {
+  res.json({ message: "Auth API working ✅" });
+});
 
 // middlewares
 app.use(express.json());
@@ -29,4 +32,5 @@ mongoose.connect(process.env.MONGO_URI)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
+
 );
